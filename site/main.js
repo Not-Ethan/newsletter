4d5a0c81cd09c9616f78e1d379e55d799828d1b2
Ethan W <ethan.wanq@gmail.com>
2024-11-20 03:25:03 +0000

    api test

--- site/main.js
+++ site/main.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Wofregiufjwoeirgido !');
+app.use(express.json());
+
+app.post('/api', (req, res) => {
+  console.log(req.body);
 })
 
 app.listen(port)
